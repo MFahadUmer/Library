077102c1f8a8743697c9da850dc29eb9e1acfe0a
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-09-02 16:17:23 +0300

    add my form on submit method

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 let myLibrary = [];
-function addBook(title, author, pages, read = false) {
- myLibrary.push([title, author, pages, read]);
+
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function takeValue (){
-  let formData = new FormData([myForm]);
-  let title = document.querySelector('#title').textContent;
-  // let author = document.getElementById('author').value;
-  // let pages = document.getElementById('pages').value;
-  console.log(`${title}`);
-}
+function addBookToLibrary (book) {
+  myLibrary.push(book);
+};
+const newBook = new Book;
+myForm.onsubmit = (e) => {
+  e.preventDefault();
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  newBook.title = title;
+  newBook.author = author;
+  newBook.pages = pages;
+  addBookToLibrary(newBook);
+  // document.getElementById('myForm').reset()
+};
 
+// let title = document.getElementById("title").value;
+
+
+
+// myForm.onsubmit = (e) => {
+//   e.preventDefault();
+//   var title = document.getElementById("title").value;
+//   var author = document.getElementById("author").value;
+//   var pages = document.getElementById("pages").value;
+//   // var read = document.getElementById("inlineRadio1").value;
+//   // var read2 = document.getElementById("inlineRadio2").value;
+// };
 let website = document.querySelector("#website").textContent;
 console.log(website);
