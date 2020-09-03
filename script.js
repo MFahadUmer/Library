3738781daf1b7c934b42c9e7aa172e4b90832b27
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-09-03 16:13:57 +0300

    add delete funtion

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-function Book(title, author, pages, read ) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -10,8 +10,9 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function delete_book(i){
+function delete_book(i) {
   myLibrary.splice(i, 1);
+  displayBooks(myLibrary);
 }
 
 let newBook;
@@ -22,34 +23,35 @@ myForm.onsubmit = (e) => {
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
   let radio = document.getElementsByName("inlineRadioOptions");
-  let radio1 = radio[0].checked? radio[0].value : radio[1].value;
+  let radio1 = radio[0].checked ? radio[0].value : radio[1].value;
   newBook = new Book(title, author, pages, radio1);
   addBookToLibrary(newBook);
   document.getElementById("myForm").reset();
   displayBooks(myLibrary);
 };
 
-document.querySelector("#modalButton").addEventListener("click", () => {
-});
-
+document.querySelector("#modalButton").addEventListener("click", () => {});
 
 function displayBooks(allbooks) {
-  let div;
-for (let i = 0; i < allbooks.length; i++)
-  {
-    div = document.createElement("div");
-    div.className = "card";
-    div.innerHTML = `
-    <div class="card-body">
-      <p class="card-text">
-     Title: ${allbooks[i].title}<br>
-     Author: ${allbooks[i].author}<br>
-     Pages: ${allbooks[i].pages}<br>
-     Read Status: ${allbooks[i].read}<br>
-     <button class="btn btn-warning" onclick="delete_book(${i})">Delete Record</button>
-     </p>
-    </div>
-  `;
-  }
+  document.querySelector("#card").innerHTML = "";
+  if (allbooks.length > 0) {
+    let div;
+    for (let i = 0; i < allbooks.length; i++) {
+      console.log(allbooks[i].title);
+      div = document.createElement("div");
+      div.className = "card";
+      div.innerHTML = `
+        <div class="card-body">
+          <p class="card-text">
+         Title: ${allbooks[i].title}<br>
+         Author: ${allbooks[i].author}<br>
+         Pages: ${allbooks[i].pages}<br>
+         Read Status: ${allbooks[i].read}<br>
+         <button class="btn btn-warning" onclick="delete_book(${i})">Delete Record</button>
+         </p>
+        </div>
+      `;
+    }
     document.querySelector(".displayBooks").appendChild(div);
+  }
 }
