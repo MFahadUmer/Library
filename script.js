e1ffea4078d392cb9fc5c455237de537b3caaf8b
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-09-04 22:02:46 +0300

    fix more eslint errors

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const myLibrary = localStorage.getItem('books')
-  ? JSON.parse(localStorage.getItem('books'))
+const myLibrary = localStorage.getItem("books")
+  ? JSON.parse(localStorage.getItem("books"))
   : [];
 
-localStorage.setItem('books', JSON.stringify(myLibrary));
-JSON.parse(localStorage.getItem('books'));
+localStorage.setItem("books", JSON.stringify(myLibrary));
+JSON.parse(localStorage.getItem("books"));
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -14,26 +14,25 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
-  localStorage.setItem('books', JSON.stringify(myLibrary));
+  localStorage.setItem("books", JSON.stringify(myLibrary));
   location.reload();
 }
 
 function toggle(index) {
-  if (myLibrary[index].read === 'false') {
-    myLibrary[index].read = 'true';
-    localStorage.setItem('books', JSON.stringify(myLibrary));
+  if (myLibrary[index].read === "false") {
+    myLibrary[index].read = "true";
+    localStorage.setItem("books", JSON.stringify(myLibrary));
     location.reload();
   } else {
-    myLibrary[index].read = 'false';
-    localStorage.setItem('books', JSON.stringify(myLibrary));
+    myLibrary[index].read = "false";
+    localStorage.setItem("books", JSON.stringify(myLibrary));
     location.reload();
   }
 }
 
-function delete_book(index) {
+function deleteBook(index) {
   myLibrary.splice(index, 1);
-  myMovie = JSON.parse(localStorage.getItem('myMovie'));
-  localStorage.setItem('books', JSON.stringify(myLibrary));
+  localStorage.setItem("books", JSON.stringify(myLibrary));
   location.reload();
 }
 
@@ -41,24 +40,24 @@ let newBook;
 
 myForm.onsubmit = (e) => {
   e.preventDefault();
-  let title = document.getElementById('title').value;
-  let author = document.getElementById('author').value;
-  let pages = document.getElementById('pages').value;
-  let radio = document.getElementsByName('inlineRadioOptions');
-  let radio1 = radio[0].checked ? radio[0].value : radio[1].value;
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const radio = document.getElementsByName("inlineRadioOptions");
+  const radio1 = radio[0].checked ? radio[0].value : radio[1].value;
   newBook = new Book(title, author, pages, radio1);
   addBookToLibrary(newBook);
-  document.getElementById('myForm').reset();
+  document.getElementById("myForm").reset();
 };
 
-document.querySelector('#modalButton').addEventListener('click', () => {});
+document.querySelector("#modalButton").addEventListener("click", () => {});
 
 function displayBooks(myLibrary) {
   let div;
 
   myLibrary.filter((obj, index) => {
-    div = document.createElement('div');
-    div.className = 'card';
+    div = document.createElement("div");
+    div.className = "card";
     div.innerHTML = `
       <div class='card-body'>
         <div class='card-text'>
@@ -68,11 +67,11 @@ function displayBooks(myLibrary) {
           <div class='d-flex flex-row'><p class='mr-1 font-weight-bold'>Read Status:</p> ${obj.read}</div> 
           <div class='d-flex justify-content-between mt-3'>
             <button class='btn-sm btn-primary' onclick='toggle(${index})'>Change status</button>
-            <button class='btn-sm btn-warning' onclick='delete_book(${index})'>Delete Record</button>
+            <button class='btn-sm btn-warning' onclick='deleteBook(${index})'>Delete Record</button>
           </div>
         </div>
       </div>
     `;
-    document.querySelector('.displayBooks').appendChild(div);
+    document.querySelector(".displayBooks").appendChild(div);
   });
 }
